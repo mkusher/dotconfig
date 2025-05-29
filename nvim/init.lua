383137a120fda144906e44edbc94e803c34a7de3
Aleh Kashnikau <akashnikau@teladochealth.com>
2025-05-29 22:30:51 +0200

    Remove nvim-cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -97,11 +97,6 @@ Plug('neovim/nvim-lspconfig')
 Plug('rafamadriz/friendly-snippets')
 Plug('Saghen/blink.cmp', {['tag'] = 'v1.3.1'})
 Plug('Saghen/blink.compat')
---Plug('hrsh7th/cmp-nvim-lsp')
---Plug('hrsh7th/cmp-buffer')
---Plug('hrsh7th/cmp-path')
---Plug('hrsh7th/cmp-cmdline')
---Plug('hrsh7th/nvim-cmp')
 Plug('zbirenbaum/copilot-cmp')
 -- }}}
 -- Project navigation {{{
@@ -170,101 +165,7 @@ require("lspconfig").yamlls.setup {
   }
 }
 require("lspconfig").protols.setup {}
-
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
---local cmp = require'cmp'
---cmp.setup({
-    --snippet = {
-      --expand = function(args)
-        --vim.snippet.expand(args.body)
-      --end,
-    --},
-    --window = {
-      ---- completion = cmp.config.window.bordered(),
-      ---- documentation = cmp.config.window.bordered(),
-    --},
-    --mapping = cmp.mapping.preset.insert({
-      --['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      --['<C-f>'] = cmp.mapping.scroll_docs(4),
-      --['<C-Space>'] = cmp.mapping.complete(),
-      --['<C-e>'] = cmp.mapping.abort(),
-      --['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      --['<S-Tab>'] = cmp.mapping(function(fallback)
-          --if cmp.visible() then
-            --cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-          --else
-            --fallback()
-          --end
-        --end, { "i", "s" }),
-      --['<Tab>'] = cmp.mapping(function(fallback)
-          --if cmp.visible() then
-            --cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-          --[> Replace with your snippet engine (see above sections on this page)
-          --elseif snippy.can_expand_or_advance() then
-            --snippy.expand_or_advance() ]]
-          --else
-            --fallback()
-          --end
-        --end, { "i", "s" }),
-    --}),
-    --formatting = {
-      --format = lspkind.cmp_format({
-        --mode = 'symbol_text', -- show only symbol annotations
-        --preset = 'default',
-        --maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-        --ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-
-        --symbol_map = {
-          --Copilot = "",
-        --},
-
-        ---- The function below will be called before any actual modifications from lspkind
-        ---- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-        --before = function (entry, vim_item)
-          --return vim_item
-        --end,
-      --})
-    --},
-    --sources = cmp.config.sources({
-      --{ name = 'copilot' },
-      --{ name = 'nvim_lsp' },
-    --}, {
-      --{ name = 'buffer' },
-    --})
---})
-
--- Set configuration for specific filetype.
---cmp.setup.filetype('gitcommit', {
---sources = cmp.config.sources({
-  --{ name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
---}, {
-  --{ name = 'buffer' },
---})
---})
-
----- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
---cmp.setup.cmdline({ '/', '?' }, {
---mapping = cmp.mapping.preset.cmdline(),
---sources = {
-  --{ name = 'buffer' }
---}
---})
-
----- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
---cmp.setup.cmdline(':', {
---mapping = cmp.mapping.preset.cmdline(),
---sources = cmp.config.sources({
-  --{ name = 'path' }
---}, {
-  --{ name = 'cmdline' }
---})
---})
-
+require("lspconfig").pyright.setup {}
 
 require("copilot").setup({
   suggestion = { enabled = false },
@@ -281,9 +182,26 @@ require("copilot_cmp").setup({
   debounce_ms = 50,
 })
 
+local has_words_before = function()
+  local col = vim.api.nvim_win_get_cursor(0)[2]
+  if col == 0 then
+    return false
+  end
+  local line = vim.api.nvim_get_current_line()
+  return line:sub(col, col):match("%s") == nil
+end
+
 require('blink.cmp').setup({
     keymap = {
         preset = 'cmdline',
+        ['<Tab>'] = {
+            function(cmp)
+              if has_words_before() then
+                return cmp.insert_next()
+              end
+            end,
+            'fallback',
+        },
         ['<CR>'] = { 'accept', 'fallback' }
     },
     sources = {
@@ -291,9 +209,28 @@ require('blink.cmp').setup({
         providers = {
             copilot = {
                 name = 'copilot',
-                module = 'blink.compat.source'
+                module = 'blink.compat.source',
+                transform_items = function(ctx, items)
+                  for _, item in ipairs(items) do
+                    item.kind_icon = ''
+                    item.kind_name = 'Copilot'
+                  end
+                  return items
+                end
             }
         }
+    },
+    cmdline = {
+      completion = {
+          menu = {
+              auto_show = true
+          },
+          list = {
+              selection = {
+                  preselect = false
+              }
+          }
+      },
     },
     completion = {
         list = {
@@ -301,12 +238,12 @@ require('blink.cmp').setup({
                 preselect = false
             }
         },
-        ghost_text = {
-          enabled = true
-        },
         documentation = {
           auto_show = true
         }
+    },
+    signature = {
+      enabled = true
     }
 })
 
